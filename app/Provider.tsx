299c2375e07f191a70c466481da55831f820c7e0
Ik <ci980704@naver.com>
2024-07-26 10:37:53 +0900

    chore: 프로젝트 초기 세팅 (#2)

---
+++ app/Provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ReactNode } from "react";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function Provider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
