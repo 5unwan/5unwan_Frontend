51ff5cf9da781a83296cb6bb0788066556e9eee7
choi-ik <ci980704@naver.com>
2024-08-03 20:39:56 +0900

    style: 아래에서 위로 천천히 올라오는 slide-up 애니메이션 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,10 +81,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        slideUp: {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-up": "slideUp 0.5s ease-out",
       },
     },
   },
