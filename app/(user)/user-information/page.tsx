7519d90601a78a2aa81ce991b1a0ed81dfe6b062
choi-ik <ci980704@naver.com>
2024-08-13 17:45:11 +0900

    style: user-information 경로의 page.tsx 구현

---
+++ app/(user)/user-information/page.tsx
@@ -0,0 +1,9 @@
+import InformationSlide from "./_components/InformationSlide";
+
+export default function UserInformation() {
+  return (
+    <main className="w-full flex-col items-center overflow-hidden">
+      <InformationSlide />
+    </main>
+  );
+}
