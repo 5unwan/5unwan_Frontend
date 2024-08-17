84346926d5a99551cd2f18f1a69efbc2a0aa3ee8
MaSeungHyun <tpdlqj0514@gmail.com>
2024-08-18 00:24:44 +0900

    chore: 기존 클릭 이벤트를 useOutsideClick  훅을 이용하여 해당 컴포넌트 마운트 관리

--- components/ActionSheet.tsx
+++ components/ActionSheet.tsx
@@ -1,11 +1,10 @@
-import React, { Dispatch, SetStateAction } from "react";
-import { Button } from "./ui/button";
+"use client";
 
-export type actionProps = {
-  name: string;
-  action: () => void;
-  type?: "danger" | "basic" | undefined;
-};
+import React, { useRef } from "react";
+import { Button } from "./ui/button";
+import { actionProps } from "@/types/actionSheet";
+import useOutsideClick from "@/hooks/useOutsideClick";
+
 interface ActionSheetProps {
   actions: actionProps[];
   closeAction: () => void;
@@ -14,13 +13,16 @@ export default function ActionSheet({
   actions,
   closeAction,
 }: ActionSheetProps) {
+  const actionsRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(actionsRef, closeAction);
+
   return (
     <div className="fixed left-0 top-0 flex h-full w-full">
+      <div className="flex h-full w-full bg-black opacity-40" />
       <div
-        className="flex h-full w-full bg-black opacity-40"
-        onClick={closeAction}
-      />
-      <div className="absolute bottom-3 flex w-full flex-col-reverse items-center justify-center">
+        ref={actionsRef}
+        className="animate-slide-up absolute bottom-3 flex w-full flex-col-reverse items-center justify-center duration-300"
+      >
         <Button
           className="h-[56px] w-11/12 items-center justify-center bg-[#25252588] p-3 text-[17px] text-[#0A84FF] backdrop-blur-sm hover:bg-[#303030]"
           onClick={closeAction}
@@ -28,19 +30,19 @@ export default function ActionSheet({
           취소
         </Button>
         <div className="relative bottom-3 w-11/12 items-center justify-center overflow-hidden rounded-[10px] backdrop-blur-sm">
-          {actions.map((action, index) => {
+          {actions.map(({ name, action, type }, index) => {
             return (
-              <div key={`action_${action.name}`}>
+              <div key={`action_${name}`}>
                 <Button
                   className="h-[56px] w-full rounded-none bg-[#25252588] text-[17px] text-white hover:bg-[#30303088]"
                   style={{
-                    color: action.type === "danger" ? "#FF453A" : "white",
+                    color: type === "danger" ? "#FF453A" : "white",
                   }}
                   onClick={() => {
-                    action.action();
+                    action();
                   }}
                 >
-                  {action.name}
+                  {name}
                 </Button>
                 {index < actions.length - 1 && (
                   <hr className="h-[1px] w-full border-[#80808070]" />
