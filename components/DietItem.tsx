c1f22c1770a93e55c4a119032ce2ecb53dcae416
MaSeungHyun <tpdlqj0514@gmail.com>
2024-08-18 00:26:45 +0900

    chore: Props명 변경 및 초기화값 삭제, 주석 제거, JS Doc제거,  css 정의 색상토큰 적용

--- components/DietItem.tsx
+++ components/DietItem.tsx
@@ -5,43 +5,26 @@ import imageSvg from "public/image.svg";
 import { Input } from "./ui/input";
 import { EllipsisVertical } from "lucide-react";
 import { useState } from "react";
-import ActionSheet, { actionProps } from "./ActionSheet";
+import { formatTime } from "@/utils/dateUtils";
+import ActionSheet from "./ActionSheet";
+import { actionProps } from "@/types/actionSheet";
 
 interface DietItemProps {
   src: string;
-  mealType: "아침" | "점심" | "저녁" | "간식" | "미입력";
+  mealType: "아침" | "점심" | "저녁" | "간식";
   mealTime: Date;
-  mealWeight: "가볍게" | "적당히" | "무겁게" | "미입력";
-  mealMass: number;
+  mealAmount: "가볍게" | "적당히" | "무겁게";
+  mealWeight: number;
   calorie: number;
 }
 
-function formatTime(date: Date): string {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  const period = hours >= 12 ? "오후" : "오전";
-  const formattedHours = (hours % 12 || 12).toString().padStart(2, "0"); // 0 시를 12 시로 표현하고 두 자리로 포맷
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-
-  return `${period} ${formattedHours}:${formattedMinutes}`;
-}
-
-/**
- * @param {string} src - 이미지 경로
- * @param {"아침" | "점심" | "저녁" | "간식"} mealTime - 식사 시간을 나타내는 명사
- * @param {string} dateTime - 식사 시간 (타임스탬프 등)
- * @param {string} mealWeight - 식사량을 나타내는 텍스트
- * @param {number} mealMass - 식사의 무게 (그램 등)
- * @param {number} calorie - 식사의 칼로리
- */
 export default function DietItem({
   src,
-  mealType = "미입력",
+  mealType,
   mealTime = new Date(),
-  mealWeight = "미입력",
-  mealMass = 0,
-  calorie = 0,
+  mealAmount,
+  mealWeight,
+  calorie,
 }: DietItemProps) {
   const [statusAction, setStatusAction] = useState(false);
 
@@ -79,7 +62,7 @@ export default function DietItem({
   return (
     <>
       <div className="flex h-[219px] w-full justify-center p-3 text-white">
-        <div className="h-1/1 flex aspect-square w-[200px] items-center justify-center rounded-[10px] bg-[#D9D9D9]">
+        <div className="min-h-[200px]h-1/1 flex aspect-square min-w-[200px] items-center justify-center rounded-[10px] bg-[#D9D9D9]">
           <Image
             src={src ? src : imageSvg}
             alt="Image"
@@ -87,26 +70,26 @@ export default function DietItem({
             height={72}
           />
         </div>
-        <div className="h-1/1 ml-[12px] flex min-w-[168px] flex-col justify-between">
+        <div className="h-1/1 ml-[12px] flex w-full min-w-[168px] flex-col justify-between">
           <article>
             <span className="text-[17px]">{mealType}</span>
             <br />
-            <span className="text-[13px] text-[#9E9E9E]">
-              {`${formatTime(mealTime)}`} ㆍ {mealWeight}
+            <span className="text-[13px] text-sub-text">
+              {`${formatTime(mealTime)}`} ㆍ {mealAmount}
             </span>
             <br />
-            <span className="text-[13px] text-[#9E9E9E]">
-              {`${mealMass}g / ${calorie}kcal `}
+            <span className="text-[13px] text-sub-text">
+              {`${mealWeight}g / ${calorie}kcal `}
             </span>
           </article>
           <Input
-            className="rounded-[10px] border-[#9E9E9E] bg-[#1E1E1E] text-[15px] placeholder:text-center placeholder:text-[#9E9E9E]"
+            className="rounded-[10px] border-sub-text bg-[#1E1E1E] text-[15px] placeholder:text-center placeholder:text-sub-text"
             placeholder="피드백을 남겨주세요"
           />
         </div>
 
         <EllipsisVertical
-          className="text-[#9E9E9E]"
+          className="text-sub-text"
           size={30}
           onClick={openAction}
         />
