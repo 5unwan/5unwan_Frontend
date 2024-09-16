82b8b89a0b6349e8257882c85fdf4ee11ea7e6c9
MaSeungHyun <tpdlqj0514@gmail.com>
2024-09-16 14:15:58 +0900

    chore: 기존 data명 식사시간과 포만감에 대한 네이밍으로 변경 및 classnames를 통한 tailwind 적용

--- app/(user)/user-information/_components/MealJounralRadioCard.tsx
+++ app/(user)/user-information/_components/MealJounralRadioCard.tsx
@@ -1,17 +1,20 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { cn } from "@/lib/utils";
+import React, { CSSProperties } from "react";
 interface MealJournalRadioCardProps {
+  className: string;
   label: string;
   type: string;
-  data: Array<{ value: string; key: string }>;
+  mealTimeAndSatiety: Array<{ value: string; key: string }>;
   select: string;
   handleClickJournalContent: (type: string, value: string) => void;
 }
 
 export default function MealJournalRadioCard({
+  className,
   label,
   type,
-  data,
+  mealTimeAndSatiety,
   select,
   handleClickJournalContent,
 }: MealJournalRadioCardProps) {
@@ -19,11 +22,14 @@ export default function MealJournalRadioCard({
     <section className="w-full">
       <p className="text-[#f5f5f5]">{label}</p>
       <section className="mt-2 flex w-full gap-2 overflow-x-scroll">
-        {data.map(({ value, key }) => {
+        {mealTimeAndSatiety.map(({ value, key }) => {
           return (
             <Button
               key={key}
-              className={`h-[34px] min-w-[61px] rounded-[13px] hover:bg-primary-user hover:text-main-bg ${select === value ? "bg-primary-user text-main-bg" : "bg-[#303030] text-sub-text"}`}
+              className={cn(
+                `h-[34px] min-w-[61px] rounded-[13px] bg-[#303030] text-sub-text hover:bg-primary-user hover:text-main-bg`,
+                select === value && className,
+              )}
               onClick={() => {
                 handleClickJournalContent(type, value);
               }}
