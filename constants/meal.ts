f963c0360ec8a17b29ddf3c0f98f6461907699bc
MaSeungHyun <tpdlqj0514@gmail.com>
2024-09-16 15:41:50 +0900

    chore: 상수는 constants 폴더에서 관리하기 위해 상수 위치 변경

---
+++ constants/meal.ts
@@ -0,0 +1,15 @@
+export const MEAL_TYPES = [
+  { value: "아침", key: "breakfast" },
+  { value: "아점", key: "brunch" },
+  { value: "점심", key: "lunch" },
+  { value: "점저", key: "linner" },
+  { value: "저녁", key: "dinner" },
+  { value: "간식", key: "snack" },
+];
+
+export const MEAL_AMOUNTS = [
+  { value: "가볍게", key: "light" },
+  { value: "배부르게", key: "full" },
+  { value: "적당히", key: "enough" },
+  { value: "과하게", key: "over" },
+];
