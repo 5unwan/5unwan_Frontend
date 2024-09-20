e883d4efcf4ed39f690948e6b8d243fce1ea90ff
MaSeungHyun <tpdlqj0514@gmail.com>
2024-09-21 01:35:12 +0900

    style: utilis/cn을 사용한 classnames 재구현

--- components/ExcerciseItem.tsx
+++ components/ExcerciseItem.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface ExcerciseItemProps {
+  className: string;
   excercise: string;
   favorite: boolean;
   select: boolean;
-  handleClickSetExcercise: (excercise: string) => void;
+  onClickSetExcercise: (excercise: string) => void;
 }
 export default function ExcerciseItem({
   excercise,
   favorite,
   select,
-  handleClickSetExcercise,
+  onClickSetExcercise,
 }: ExcerciseItemProps) {
   return (
     <section
-      className={`flex min-h-[46px] w-full items-center justify-between rounded-[10px] ${select ? "text bg-primary-user text-sub-bg" : "bg-[#303030]"} px-5`}
+      className={cn(
+        "flex min-h-[46px] w-full items-center justify-between rounded-[10px] bg-[#303030] px-5",
+        {
+          "bg-primary-user text-sub-bg": select,
+        },
+      )}
       onClick={() => {
-        handleClickSetExcercise(excercise);
+        onClickSetExcercise(excercise);
       }}
     >
       <span>{excercise}</span>
       <Heart
-        className={`h-[22px] w-[22px] ${select ? (favorite ? "fill-[#303030]" : "text-[#303030]") : favorite ? "fill-sub-text text-sub-text" : "text-sub-text"} hover:cursor-pointer"`}
+        className={cn("h-[22px] w-[22px] hover:cursor-pointer", {
+          "fill-[#303030]": select && favorite,
+          "text-[#303030]": select && !favorite,
+          "fill-sub-text text-sub-text": !select && favorite,
+          "text-sub-text": !select && !favorite,
+        })}
       />
     </section>
   );
