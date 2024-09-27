3ed6029379b8676c4fe7ce2051a26ad0d7a2173e
MaSeungHyun <tpdlqj0514@gmail.com>
2024-09-27 22:49:58 +0900

    style: 합성 컴포넌트를 활용한 화면 하단 액션 시트 구현

--- components/ActionSheet.tsx
+++ components/ActionSheet.tsx
@@ -1,57 +1,68 @@
 "use client";
 
-import React, { useRef } from "react";
-import { ActionProps } from "@/types/actionSheet";
+import React, { forwardRef, ReactNode, useEffect, useRef } from "react";
+import { ActionProps as ActionPropsType } from "@/types/actionSheet";
 import useOutsideClick from "@/hooks/useOutsideClick";
 import { Button } from "./ui/button";
+import { createPortal } from "react-dom";
+import { cn } from "@/lib/utils";
 
-interface ActionSheetProps {
-  actions: ActionProps[];
-  closeAction: () => void;
+interface ItemProps {
+  label: string;
+  onClick?: () => void;
+  className?: string;
 }
-export default function ActionSheet({
-  actions,
-  closeAction,
-}: ActionSheetProps) {
-  const actionsRef = useRef<HTMLDivElement>(null);
-  useOutsideClick(actionsRef, closeAction);
 
+function Item({ label, onClick, className }: ItemProps) {
   return (
-    <div className="fixed left-0 top-0 flex h-full w-full">
-      <div className="flex h-full w-full bg-black opacity-40" />
-      <div
-        ref={actionsRef}
-        className="absolute bottom-3 flex w-full animate-slide-up flex-col-reverse items-center justify-center duration-300"
-      >
-        <Button
-          className="h-[56px] w-11/12 items-center justify-center bg-[#25252588] p-3 text-[17px] text-[#0A84FF] backdrop-blur-sm hover:bg-[#303030]"
-          onClick={closeAction}
-        >
-          취소
-        </Button>
-        <div className="relative bottom-3 w-11/12 items-center justify-center overflow-hidden rounded-[10px] backdrop-blur-sm">
-          {actions.map(({ name, action, type }, index) => {
-            return (
-              <div key={`action_${name}`}>
-                <Button
-                  className="h-[56px] w-full rounded-none bg-[#25252588] text-[17px] text-white hover:bg-[#30303088]"
-                  style={{
-                    color: type === "danger" ? "#FF453A" : "white",
-                  }}
-                  onClick={() => {
-                    action();
-                  }}
-                >
-                  {name}
-                </Button>
-                {index < actions.length - 1 && (
-                  <hr className="h-[1px] w-full border-[#80808070]" />
-                )}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    <Button
+      className={cn(
+        `h-[56px] w-full rounded-none bg-sub-bg text-[17px] font-normal text-white hover:bg-[#30303088]`,
+        className,
+      )}
+      onClick={onClick}
+    >
+      {label}
+    </Button>
   );
 }
+
+interface ActionSheetProps {
+  children: ReactNode;
+  onClickCloseActionSheet: () => void;
+  ref: React.RefObject<HTMLDivElement>;
+}
+
+const ActionSheetContainer = forwardRef<HTMLDivElement, ActionSheetProps>(
+  function ActionSheetContainer(
+    { children, onClickCloseActionSheet }: ActionSheetProps,
+    ref,
+  ) {
+    return createPortal(
+      <div ref={ref} className="fixed left-0 top-0 flex h-full w-full">
+        <div
+          className="flex h-full w-full bg-black opacity-40"
+          onClick={onClickCloseActionSheet}
+        />
+        <div className="absolute bottom-3 flex w-full animate-slide-up flex-col-reverse items-center justify-center duration-300">
+          <Button
+            className="h-[56px] w-11/12 items-center justify-center bg-sub-bg p-3 text-[17px] text-[#0A84FF] backdrop-blur-sm hover:bg-[#303030]"
+            onClick={onClickCloseActionSheet}
+          >
+            취소
+          </Button>
+          <div className="relative bottom-3 w-11/12 items-center justify-center divide-y divide-[#5b5b5b] overflow-hidden rounded-[10px] font-light backdrop-blur-sm">
+            {children}
+          </div>
+        </div>
+      </div>,
+      document.body,
+    );
+  },
+);
+
+const ActionSheet = Object.assign(ActionSheetContainer, {
+  Item,
+});
+
+export default ActionSheet;
